e4e1f5bbd379a46729a66230aa4fca575229b670
Eric Janowski <ejanow@gmail.com>
2016-06-20 16:58:34 -0500

    final cleanup

--- js/app.js
+++ js/app.js
@@ -8,13 +8,15 @@ $(document).ready(function(){
     var inpt = $('input[name=date]').val();
     var inptInMS = new Date(inpt).getTime();
 
-    var daysBetween = Math.ceil((inptInMS - today) / MS_IN_DAY);
+    var daysBetween = Math.abs(Math.ceil((inptInMS - today) / MS_IN_DAY));
 
     var plural;
     daysBetween > 1 ? plural = ' are: ' : plural = ' is: ';
 
-    var result = "The number of days between today and "
-                 + inpt + plural + daysBetween;
-    $('h2#result').text(result);
+    var result = "The number of days between today and " +
+                 inpt + plural + daysBetween;
+    $('h3#result').text(result);
+
   });
+
 });
