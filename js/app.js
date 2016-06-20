3eb99159d19e0fa86f0d79e1797b552a3844b3ee
Eric Janowski <ejanow@gmail.com>
2016-06-20 16:24:53 -0500

    initial logic

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  $('button#inpt').click(function(){
+
+    const MS_IN_DAY = 86400000;
+
+    var today = new Date().getTime();
+    var inpt = $('input[name=date]').val();
+    var inptInMS = new Date(inpt).getTime();
+
+    var daysBetween = Math.ceil((inptInMS - today) / MS_IN_DAY);
+
+    var plural;
+    daysBetween > 1 ? plural = ' are: ' : plural = ' is: ';
+
+    var result = "The number of days between today and "
+                 + inpt + plural + daysBetween;
+    $('h2#result').text(result);
+  });
+});
